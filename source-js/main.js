9ba4f2fb119149563c6f83cf6b72824b571d9c46
Валерійка <kudravka13@gmail.com>
2017-12-09 01:32:51 +0200

    Added close block

--- source-js/main.js
+++ source-js/main.js
@@ -1,16 +1,26 @@
 var input = document.getElementsByTagName("body"),
     i = 0;
 
-document.getElementById("body").addEventListener("click", function (event) {
-    console.log('Bla');
+document.getElementById("body").addEventListener("dblclick", function (event) {
     var newItem_li = document.createElement("div");
-     newItem_li.innerHTML = ' <p>Bla</p>';
+    newItem_li.innerHTML = '<div class="content"><p>Bla</p></div>';
+    newItem_li.appendChild(document.createElement("div")).classList.add('close');
     i++;
     newItem_li.id = i;
     newItem_li.classList.add('item');
     document.getElementById("body").appendChild(newItem_li);
 
-    newItem_li.style.top =  event.clientY - document.getElementById("body").offsetTop - 10 + "px";
-    newItem_li.style.left =  event.clientX - document.getElementById("body").offsetLeft - 20 + "px";
+    newItem_li.style.top = event.clientY - document.getElementById("body").offsetTop - 10 + "px";
+    newItem_li.style.left = event.clientX - document.getElementById("body").offsetLeft - 20 + "px";
+})
+
+document.getElementById("body").addEventListener("click", function (event) {
+    if (!event.target.parentNode.parentNode.classList.contains('active')) {
+        event.target.parentNode.parentNode.classList.add('active');
+    } else {
+        event.target.parentNode.parentNode.classList.remove('active');
+    }
+
+    console.log(event.target.parentNode);
 })
 
