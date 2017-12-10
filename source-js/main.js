8d1585756ec1e1539918b520d8f0c60ccb6aedb0
Валерійка <kudravka13@gmail.com>
2017-12-10 18:36:46 +0200

    minor

--- source-js/main.js
+++ source-js/main.js
@@ -2,15 +2,16 @@ var input = document.getElementsByTagName("body"),
     i = 0;
 
 document.getElementById("body").addEventListener("dblclick", function (event) {
-    var newItem_li = document.createElement("div");
+    var newItem_li = document.createElement("li");
     newItem_li.innerHTML = '<div class="content"><p>Bla</p></div> <div class="close" onclick="del(this)"></div>';
     i++;
     newItem_li.id = i;
     newItem_li.classList.add('item');
     document.getElementById("body").appendChild(newItem_li);
 
-    newItem_li.style.top = event.clientY - document.getElementById("body").offsetTop - 10 + "px";
+    newItem_li.style.top = event.clientY - document.getElementById("body").offsetTop + 15 + "px";
     newItem_li.style.left = event.clientX - document.getElementById("body").offsetLeft - 20 + "px";
+
 })
 
 document.getElementById("body").addEventListener("click", function (event) {
